6426ae53b55f13934e04d711bbcdb2e9cd880178
ss24610 <111543669+ss24610@users.noreply.github.com>
2025-06-07 22:53:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,16 +9,29 @@ from copy import deepcopy
 from heap import MinHeap, HeapNode
 
 #default goal state, 0 represents empty cell
+#will be used in comparisons for the A* search
 goal = [
     [0,1,2],
     [3,4,5],
     [6,7,8]
 ]
 
+def state_to_tuple(state):
+    return tuple(tuple(row) for row in state)
+
+
 #compares current state to the goal state
 def goal_state(state):
     return state==goal 
 
+#converts a state to 1D representation for easier comparison
+def convert_to_1D(state):
+    list = []
+    for row in state:
+        for element in row:
+            list.append(element)
+    
+    return list
 
 #heuristic one determines the number of misplaced tiles
 def heuristic_one(state):
@@ -30,15 +43,15 @@ def heuristic_one(state):
     
     return count 
 
-#heuristic two determines the (concept name i forgot) distance
+# heuristic two determines the (concept name i forgot) distance
 # it was manhattan distance
 def heuristic_two(state):
 
     positions = {}
-
+    distance = 0
     for i in range(3):
         for j in range(3):
-            positions[goal[i][j]] = (i,j)
+            positions[goal[i][j]] = (j,i)
 
 
     for i in range(3):
@@ -46,7 +59,7 @@ def heuristic_two(state):
             value = state[i][j]
             gx,gy = positions[value]
 
-            distance = abs(gx-j) + abs(gy-i)
+            distance += abs(gx-j) + abs(gy-i)
 
     return distance
 
@@ -56,14 +69,14 @@ def heuristic_two(state):
 def gen_state():
 
     while(1):
-        starting_state = [[],[],[]]
+        starting_state = [ [],[],[] ]
         numbers = [0,1,2,3,4,5,6,7,8]
 
         for i in range(3):
             for j in range(3):
                 r_num = randint(0,len(numbers)-1)
                 value = numbers.pop(r_num)
-                starting_state[i][j] = value 
+                starting_state[i].append(value) 
 
         if(valid_state(starting_state)):
             return starting_state
@@ -76,17 +89,21 @@ def valid_state(state):
 
     for i in range(3):
         for j in range(3):
-            sequence.append(state[i][j])
+            if(state[i][j]!=0):
+                sequence.append(state[i][j])
     
-    for i in range(1,9):
-        if sequence[i] < sequence[i-1]:
-            inversions += 1
+    for i in range(len(sequence)):
+        for j in range(i+1,len(sequence)):
+            if sequence[i] > sequence[j]:
+                inversions += 1
 
     return inversions % 2 == 0
 
+
+
 def generate_neighbours(state):
     neighbours = []
-    moves = { (0,1), (0,-1), (1,0), (-1,0) }
+    moves = [ (0,1), (0,-1), (1,0), (-1,0) ]
 
     for i in range(3):
         for j in range(3):
@@ -108,8 +125,6 @@ def generate_neighbours(state):
             neighbours.append(temp_state)
 
 
-
-
     return neighbours
 
 
@@ -119,24 +134,34 @@ def a_star_search(initial_state, heuristic):
     frontier = MinHeap(start_node)
     explored = set()
 
-    while(not frontier.isempty()):
+    while(not frontier.is_empty()):
         removed_heap_node = frontier.delete()
         state = removed_heap_node.state
         path = removed_heap_node.path
         g_value = removed_heap_node.g + 1
 
 
-        explored.add(str(state))
+        #explored.add(str(state))
+        state_tup = state_to_tuple(state)
+        explored.add(state_tup)
+        
 
         if(goal_state(state)):
             
-            return path + [str(state)]
+            #return path + [str(state)], len(explored)
+            return path + [state_tup], len(explored)
         
         else:
-            neighbour_path = path + [str(state)]
+            #neighbour_path = deepcopy(path) + [str(state)]
+            neighbour_path = deepcopy(path) + [state_tup]
 
             for neighbour in generate_neighbours(state):
-                if not frontier.search_node(neighbour) and str(neighbour) not in explored:
+                neighbour_tup = state_to_tuple(neighbour)
+
+                #if not frontier.search_node(neighbour) and str(neighbour) not in explored:
+                if not frontier.search_node(neighbour) and not neighbour_tup in explored:
+                #if not frontier.search_node(neighbour) and not neighbour in explored:
+                    
                     f_value = g_value+heuristic(neighbour)
                     hnode = HeapNode(f_value,g_value,neighbour, neighbour_path)
                     frontier.insert(hnode)
@@ -145,33 +170,47 @@ def a_star_search(initial_state, heuristic):
                     f_value = g_value+heuristic(neighbour)
                     frontier.decrease_key(neighbour,f_value,g_value,neighbour_path)
 
-    return None
+    return None, None
 
 def main():
 
-    h1_avg = 0
-    h1_steps = []
+    h1_avg_expands = 0
+    h1_avg_steps = 0
 
-    h2_avg = 0
-    h2_steps = []
+    h2_avg_expands = 0
+    h2_avg_steps = 0
 
     for i in range(100):
         initial_state = gen_state()
 
-        h1 = a_star_search(deepcopy(initial_state),heuristic_one)
-        h2 = a_star_search(deepcopy(initial_state),heuristic_two)
+        h1s,h1e = a_star_search(deepcopy(initial_state), heuristic_one)
+        h2s,h2e = a_star_search(deepcopy(initial_state), heuristic_two)
         #same for h3
 
-        h1_steps.append(len(h1))
-        h2_steps.append(len(h2))
+        h1_avg_expands += h1e
+        h1_avg_steps += len(h1s)
 
-        h1_avg += len(h1)
-        h2_avg += len(h2)
+        h2_avg_expands += h2e
+        h2_avg_steps += len(h2s)
     
-    h1_avg/= 100
-    h2_avg/=100
+    h1_avg_expands /=100
+    h1_avg_steps /=100
+
+    h2_avg_expands /=100
+    h2_avg_steps /=100 
+    print(f"""
+H1 avg steps = {h1_avg_steps}
+H1 avg expands = {h1_avg_expands}
+H2 avg steps = {h2_avg_steps}
+H2 avg expands = {h2_avg_expands}
+
+""")
 
 
+""""""
+initial_state = gen_state()
 
+h1s,h1e = a_star_search(initial_state, heuristic_one)
 
-
+print(len(h1s))
+print(h1e)
